9dc259f64f247729307956544bb9f01f0473a49c
hunganhngo <ngohunganh39@gmail.com>
2025-02-20 18:19:57 +0700

    hell3

--- thuchanh/buoi1/Bai1_Bai2.java
+++ thuchanh/buoi1/Bai1_Bai2.java
@@ -3,5 +3,6 @@ package buoi1;
 public class Bai1_Bai2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Minh Thien ngu");
     }
 }
